d5204556bf616edd3940185d2f3fbb9f41662d31
Meenatchisundharam M <sundharmadhim@gmail.com>
2024-09-19 12:57:03 +0530

    chor: added tailwindcss and fontawesome cdn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,4 +5,14 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss'
   ],
+  app:{
+    head:{
+      link:[
+        {
+          rel: "stylesheet",
+          href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+        },
+      ],
+    },
+  },
 });
